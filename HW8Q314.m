1a2c41a38f9686637a564e002db84cedb89d23a6
Daniel Opdahl <dopdahl16@gmail.com>
2019-10-28 16:57:28 -0500

    Add files via upload

--- HW8Q314.m
+++ HW8Q314.m
@@ -1,24 +1,39 @@
-wavelengths = [500,450,400,350,300,250]
-stopping_potentials = [0.19,0.48,0.83,1.28,1.89,2.74]
+%% HW8Q314.m
+%
+% Author: Daniel Opdahl
+% Last modified: 10/28/19
+
+x_axis_frequencies = -1:2*10^14:12*10^14;
+wavelengths = [500,450,400,350,300,250];
+stopping_potentials = [0.19,0.48,0.83,1.28,1.89,2.74];
 frequencies = [];
 i = 1;
 for a = wavelengths
-    disp(a)
     freq = (3*10^8)/(a * 10^-9);
     frequencies(i) = freq;
     i = i + 1;
 end
-hold on
-plot(frequencies, stopping_potentials)
-xlim([0 12*10^14])
-ylim([-5 3])
-disp(frequencies)
+
+x1 = frequencies;
+y1 = stopping_potentials;
+
+%Curve fitting
+myfittype1 = fittype('poly1');
+myfit1 = fit(x1',y1',myfittype1);
+
+coeffvals = coeffvalues(myfit1);
+intercept = coeffvals(2);
+slope = coeffvals(1);
+
+work_function = -1*intercept % Units of eV
+e = 1.60217662 * 10^-19;
+
 i = 1;
-for b = frequencies -1
-    run = frequencies (i+1) - frequencies(i)
-    rise = stopping_potentials(i+1) - stopping_potentials(i)
+tot = 0;
+for voltage = stopping_potentials
+    h = ((e* voltage) + work_function)/(frequencies(i));
+    tot = tot + h;
+    i = i + 1;
 end
-% rise and run are always the same
-x = -1:2*10^14:12*10^14
-y = (rise/run)*x - 2.45
-plot(x,y)
+
+experimental_val_of_h = tot/6 % Units of eV*s
