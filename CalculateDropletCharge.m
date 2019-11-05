9c51a6a1e75efea27d9f180896d1568d2346929f
Daniel Opdahl <dopdahl16@gmail.com>
2019-11-04 18:25:21 -0600

    Add files via upload

--- CalculateDropletCharge.m
+++ CalculateDropletCharge.m
@@ -17,27 +17,164 @@
 % % % temp = answer(3); %(C)
 
 
-% Input data, convert units, and define constants
+% Create array for charge values to be stored in
 
-velocity_down = 0.187577; %(mm/s)
-velocity_up = 0.306450; %(mm/s)
-temp = 19; %(C)
-plate_separation = 0.001; %(m)
-voltage = 200; %(volts)
+collected_charges = [];
 
-velocity_down = velocity_down * 0.001; %(m/s)
-velocity_up = velocity_up * 0.001; %(m/s)
+% Manually inputted data for Tom
+down_velos = [];
+up_velos = [];
+measured_temp = ;
+measured_voltage = ;
 
-density_oil = 886; %(kg/m^3)
-g = 9.81; %(m/s^2)
+% Use a for loop to run through all our manually inputted data
 
-viscosity_air = 0.00475*temp + 1.7288; %(Nsm^-2 * 10^-5)
-viscosity_air = viscosity_air * 10^-5; %(Nsm^-2)
+for i = 1:length(down_velos)
+    
+    % Input data, convert units, and define constants
 
-% Calculate droplet radius
+    velocity_down = down_velos(i); %(mm/s)
+    velocity_up = up_velos(i); %(mm/s)
+    temp = measured_temp; %(C)
+    plate_separation = 0.001; %(m)
+    voltage = measured_voltage; %(volts)
 
-sphere_radius = sqrt( (9*viscosity_air*velocity_down) / (2*density_oil*g) )
+    velocity_down = velocity_down * 0.001; %(m/s)
+    velocity_up = velocity_up * 0.001; %(m/s)
 
-% Calculate charge on droplet
+    density_oil = 886; %(kg/m^3)
+    g = 9.81; %(m/s^2)
 
-charge = (6*pi*viscosity_air*sphere_radius* (velocity_up + velocity_down) *plate_separation) / (voltage)
+    viscosity_air = 0.00475*temp + 1.7288; %(Nsm^-2 * 10^-5)
+    viscosity_air = viscosity_air * 10^-5; %(Nsm^-2)
+
+    % Calculate droplet radius
+
+    sphere_radius = sqrt( (9*viscosity_air*velocity_down) / (2*density_oil*g) )
+
+    % Calculate charge on droplet and add to collection
+
+    charge = (6*pi*viscosity_air*sphere_radius* (velocity_up + velocity_down) *plate_separation) / (voltage)
+    collected_charges.append(charge)
+    
+end
+
+% Manually inputted data for Frankenstein
+down_velos = [];
+up_velos = [];
+measured_temp = ;
+measured_voltage = ;
+
+% Use a for loop to run through all our manually inputted data
+
+for i = 1:length(down_velos)
+    
+    % Input data, convert units, and define constants
+
+    velocity_down = down_velos(i); %(mm/s)
+    velocity_up = up_velos(i); %(mm/s)
+    temp = measured_temp; %(C)
+    plate_separation = 0.001; %(m)
+    voltage = measured_voltage; %(volts)
+
+    velocity_down = velocity_down * 0.001; %(m/s)
+    velocity_up = velocity_up * 0.001; %(m/s)
+
+    density_oil = 886; %(kg/m^3)
+    g = 9.81; %(m/s^2)
+
+    viscosity_air = 0.00475*temp + 1.7288; %(Nsm^-2 * 10^-5)
+    viscosity_air = viscosity_air * 10^-5; %(Nsm^-2)
+
+    % Calculate droplet radius
+
+    sphere_radius = sqrt( (9*viscosity_air*velocity_down) / (2*density_oil*g) )
+
+    % Calculate charge on droplet and add to collection
+
+    charge = (6*pi*viscosity_air*sphere_radius* (velocity_up + velocity_down) *plate_separation) / (voltage)
+    collected_charges.append(charge)
+    
+end
+
+% Manually inputted data for Eric
+down_velos = [];
+up_velos = [];
+measured_temp = ;
+measured_voltage = ;
+
+% Use a for loop to run through all our manually inputted data
+
+for i = 1:length(down_velos)
+    
+    % Input data, convert units, and define constants
+
+    velocity_down = down_velos(i); %(mm/s)
+    velocity_up = up_velos(i); %(mm/s)
+    temp = measured_temp; %(C)
+    plate_separation = 0.001; %(m)
+    voltage = measured_voltage; %(volts)
+
+    velocity_down = velocity_down * 0.001; %(m/s)
+    velocity_up = velocity_up * 0.001; %(m/s)
+
+    density_oil = 886; %(kg/m^3)
+    g = 9.81; %(m/s^2)
+
+    viscosity_air = 0.00475*temp + 1.7288; %(Nsm^-2 * 10^-5)
+    viscosity_air = viscosity_air * 10^-5; %(Nsm^-2)
+
+    % Calculate droplet radius
+
+    sphere_radius = sqrt( (9*viscosity_air*velocity_down) / (2*density_oil*g) )
+
+    % Calculate charge on droplet and add to collection
+
+    charge = (6*pi*viscosity_air*sphere_radius* (velocity_up + velocity_down) *plate_separation) / (voltage)
+    collected_charges.append(charge)
+    
+end
+
+% Manually inputted data for Mufasa
+down_velos = [];
+up_velos = [];
+measured_temp = ;
+measured_voltage = ;
+
+% Use a for loop to run through all our manually inputted data
+
+for i = 1:length(down_velos)
+    
+    % Input data, convert units, and define constants
+
+    velocity_down = down_velos(i); %(mm/s)
+    velocity_up = up_velos(i); %(mm/s)
+    temp = measured_temp; %(C)
+    plate_separation = 0.001; %(m)
+    voltage = measured_voltage; %(volts)
+
+    velocity_down = velocity_down * 0.001; %(m/s)
+    velocity_up = velocity_up * 0.001; %(m/s)
+
+    density_oil = 886; %(kg/m^3)
+    g = 9.81; %(m/s^2)
+
+    viscosity_air = 0.00475*temp + 1.7288; %(Nsm^-2 * 10^-5)
+    viscosity_air = viscosity_air * 10^-5; %(Nsm^-2)
+
+    % Calculate droplet radius
+
+    sphere_radius = sqrt( (9*viscosity_air*velocity_down) / (2*density_oil*g) )
+
+    % Calculate charge on droplet and add to collection
+
+    charge = (6*pi*viscosity_air*sphere_radius* (velocity_up + velocity_down) *plate_separation) / (voltage)
+    collected_charges.append(charge)
+    
+end
+    
+% Take collected charges and display them in a histogram
+
+% p=[1,1,1,2,4,5,5,5,5,5,8,10];
+% h = histogram(p)
+h = histogram(collected_charges)
